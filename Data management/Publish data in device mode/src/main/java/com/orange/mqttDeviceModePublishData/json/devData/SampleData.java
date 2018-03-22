66b38cc1f670a9474a8b92274cb6c8c6da050e22
Live Objects <liveobjects.contact@orange.com>
2018-03-22 15:00:25 +0100

    Update to make useable for eventProcessing testing

--- Data management/Publish data in device mode/src/main/java/com/orange/mqttDeviceModePublishData/json/devData/SampleData.java
+++ Data management/Publish data in device mode/src/main/java/com/orange/mqttDeviceModePublishData/json/devData/SampleData.java
@@ -2,6 +2,6 @@ package com.orange.mqttDeviceModePublishData.json.devData;
 
 public class SampleData {
 	public String payload;
-	public String temp;
-	public String humidity;
+	public String temperature;
+	public String hygrometry;
 }
