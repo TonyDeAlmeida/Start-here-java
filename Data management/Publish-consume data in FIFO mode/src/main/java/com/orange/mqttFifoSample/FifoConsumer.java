d896b1a70f3dac555677105b17b8dd7849a43fef
Live Objects <liveobjects.contact@orange.com>
2018-04-04 21:23:22 +0200

    Update FifoConsumer.java

--- Data management/Publish-consume data in FIFO mode/src/main/java/com/orange/mqttFifoSample/FifoConsumer.java
+++ Data management/Publish-consume data in FIFO mode/src/main/java/com/orange/mqttFifoSample/FifoConsumer.java
@@ -29,7 +29,7 @@ public class FifoConsumer {
 
 	// Connection parameters
 		public static String SERVER    = "tcp://liveobjects.orange-business.com:1883"; // declare Live Objects end point
-		public static String API_KEY   = "f6422939005946619dd8a2575db3cb76";           // <-- REPLACE by YOUR API_KEY!	
+		public static String API_KEY   = "enter your api key here";           // <-- REPLACE by YOUR API_KEY!	
 		public static String USERNAME  ="payload+bridge";							   // The option to publish in device mode
 		public static String CLIENT_ID ="app:" + UUID.randomUUID().toString();	       // in bridge mode : should be of your choice
 		
