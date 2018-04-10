f8844d1341fc94dd557d73cccae7cfc39c419b74
Live Objects <liveobjects.contact@orange.com>
2018-04-10 16:25:52 +0200

    Update SampleData.java

--- Data management/Publish data in device mode/src/main/java/com/orange/mqttDeviceModePublishData/json/devData/SampleData.java
+++ Data management/Publish data in device mode/src/main/java/com/orange/mqttDeviceModePublishData/json/devData/SampleData.java
@@ -2,6 +2,6 @@ package com.orange.mqttDeviceModePublishData.json.devData;
 
 public class SampleData {
 	public String payload;
-	public String temperature;
-	public String hygrometry;
+	public int temperature;
+	public int hygrometry;
 }
