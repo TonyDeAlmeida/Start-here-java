b89e6571e08c7492ee9a1a4c9fd58c33a8cb7ea1
Live Objects <liveobjects.contact@orange.com>
2018-04-26 16:56:00 +0200

    Add files via upload

---
+++ Rule engine/Event processing/src/main/java/com/orange/eventProcessingSample/util/json/devData/LoFiringReturn.java
@@ -0,0 +1,10 @@
+package com.orange.eventProcessingSample.util.json.devData;
+
+public class LoFiringReturn {
+	public String id;
+	public String name ; 		
+	public boolean enabled;				
+	public Object matchingRuleIds;
+	public Object aggregationKeys;
+	public String firingType;
+}
