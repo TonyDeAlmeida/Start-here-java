9dc32a51154d236ae0a9cd315d85c6c1594383d3
Live Objects <liveobjects.contact@orange.com>
2018-05-02 12:02:13 +0200

    Create ApiKey.java

---
+++ ApiKey management/createApiKey/src/main/java/com/orange/createApiKeySample/json/ApiKey.java
@@ -0,0 +1,20 @@
+package com.orange.createApiKeySample.json;
+
+public class ApiKey {
+	public String id;
+	public String tenantId;
+	public String parentId;
+	public boolean active;
+	public String label;
+	public String description;
+	public String[] roles;
+	public String nonce;
+	public String value;
+	public String creationTs;
+	public String lastActivity;
+	public Object rateLimit;
+	public boolean sessionKey;
+	public boolean usurpedKey;
+	public boolean expired;
+	public boolean masterKey;
+}
